84e6a4a34f2259556c11fe891c489430bfa329d7
Mario Moser <mosermauer@gmail.com>
2023-09-21 14:43:46 +0200

    V5

--- datafetch.h
+++ datafetch.h
@@ -12,7 +12,7 @@ public:
     std::string response;
 
 private:
-    const std::string data_url = "https://odre.opendatasoft.com/api/explore/v2.1/catalog/datasets/eco2mix-national-tr/records?select=taux_co2%20%2C%20date_heure&limit=100";
+    const std::string data_url = "https://odre.opendatasoft.com/api/explore/v2.1/catalog/datasets/eco2mix-national-tr/records?select=taux_co2%20%2C%20date%2C%20date_heure&limit=100";
     const std::string json_filename = "CO2_data.json";
 
 };
